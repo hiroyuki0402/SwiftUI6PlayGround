03d0f4f3800f764a5dbdb191e050509ae1840f76
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2024-09-20 06:51:49 +0900

    初期画面の変更

--- SwiftUI6PlayGround/SwiftUI6PlayGroundApp.swift
+++ SwiftUI6PlayGround/SwiftUI6PlayGroundApp.swift
@@ -13,8 +13,7 @@ struct SwiftUI6PlayGroundApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            Home()
         }
     }
 }
